a3e8449e2be7f227f7269a4b9cf9973f1f7988a6
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-11-07 09:35:56 +0200

    АТД опашка

---
+++ queue/queue.h
@@ -0,0 +1,22 @@
+/*
+ АТД опашка
+*/
+
+template <typename T>
+class Queue {
+public:
+  // проверка за празнота на опашка
+  virtual bool empty() const = 0;
+
+  // включване на елемент на края на опашката
+  virtual bool enqueue(T const&) = 0;
+
+  // изключване на елемент от началото на опашката
+  virtual T dequeue() = 0;
+
+  // достъп до първия елемент на опашката
+  virtual T head() const = 0;
+
+  // достъп до първия елемент на опашката с възможност за промяна
+  virtual T& head() = 0;
+};
