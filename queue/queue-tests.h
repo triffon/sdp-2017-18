ca57eba4e5c102fd6ab28a77024f918501afbb92
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-11-07 09:55:05 +0200

    Тест за "препълване" на масива

--- queue/queue-tests.h
+++ queue/queue-tests.h
@@ -2,25 +2,25 @@
 
 // ще смятаме, че шаблонът, който тестваме е TestQueue
 
-TEST_CASE("Queue", "CreateEmptyQueue") {
+TEST_CASE("Queue", Queue_CreateEmptyQueue) {
   const TestQueue<int> q;
   Assert::IsTrue(q.empty());
 }
 
-TEST_CASE("Queue", "EnqueueNotEmpty") {
+TEST_CASE("Queue", Queue_EnqueueNotEmpty) {
   TestQueue<int> q;
   q.enqueue(5);
   Assert::IsTrue(!q.empty());
 }
 
-TEST_CASE("Queue", "EnqueueHead") {
+TEST_CASE("Queue", Queue_EnqueueHead) {
   TestQueue<int> q;
   for(int i = 1; i <= 100; i++)
     q.enqueue(i);
   Assert::AreEqual(q.head(), 1);
 }
 
-TEST_CASE("Queue", "EnqueueDequeue") {
+TEST_CASE("Queue", Queue_EnqueueDequeue) {
   TestQueue<int> q;
   for(int i = 1; i <= 100; i++)
     q.enqueue(i);
@@ -29,3 +29,12 @@ TEST_CASE("Queue", "EnqueueDequeue") {
   Assert::IsTrue(q.empty());
 }
   
+TEST_CASE("Queue", Queue_DequeueAfterEnqueue) {
+  TestQueue<int> q;
+  for(int i = 1; i <= 1000; i++) {
+    q.enqueue(i);
+    Assert::AreEqual(q.dequeue(), i);
+  }
+  Assert::IsTrue(q.empty());
+}
+    
