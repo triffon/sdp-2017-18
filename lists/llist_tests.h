1c1edc170161bef1e331e5d3aa13b5f0fd89cca2
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-11-21 10:48:26 +0200

    Готови сме с класа LinkedList

--- lists/llist_tests.h
+++ lists/llist_tests.h
@@ -21,11 +21,8 @@ TEST_CASE("LinkedList", InsertEndNotEmptyList) {
 TEST_CASE("LinkedList", InsertEndTraverseList) {
   LinkedList<int> l;
   for(int i = 0; i < 10; i++)
-    // l.insertEnd(i);
     l += i;
   int i = 0;
-  //  for(LinkedListIterator<int> it = l.begin(); it.valid(); it = it.next(), i++)
-  // Assert::AreEqual(it.get(), i);
   for(LinkedListIterator<int> it = l.begin(); it; it++, i++)
     Assert::AreEqual(*it, i);
 }
@@ -50,5 +47,107 @@ TEST_CASE("LinkedList", InsertAfterIsCorrect) {
   // l.print();
   int i = 0;
   for(LinkedListIterator<int> it = l.begin(); it; it++, i++)
+    Assert::AreEqual(*it, i);
+}
+
+TEST_CASE("LinkedList", InsertBeforeIsCorrect) {
+  LinkedList<int> l;
+  // добавяме всички четни числа
+  for(int i = 2; i < 10; i += 2)
+    l.insertEnd(i);
+  for(LinkedListIterator<int> it = l.begin(); it; ++it) {
+    // преди всяко четно вмъкваме нечетно
+    l.insertBefore(*it - 1, it);
+  }
+  // l.print();
+  int i = 1;
+  for(LinkedListIterator<int> it = l.begin(); it; it++, i++)
+    Assert::AreEqual(*it, i);
+}
+
+TEST_CASE("LinkedList", DeleteAfterIsCorrect) {
+  LinkedList<int> l;
+  for(int i = 0; i < 10; i++)
+    l += i;
+  // изтриваме всички нечетни числа
+  int i = 1;
+  for(LinkedListIterator<int> it = l.begin(); it; ++it, ++(++i)) {
+    int x;
+    l.deleteAfter(x, it);
+    Assert::AreEqual(x, i);
+  }
+  Assert::AreEqual(*l.end(), 8);
+  // останали са само четните числа
+  i = 0;
+  for(LinkedListIterator<int> it = l.begin(); it; it++, ++(++i))
+    Assert::AreEqual(*it, i);
+}
+
+TEST_CASE("LinkedList", DeleteAtIsCorrect) {
+  LinkedList<int> l;
+  for(int i = 0; i < 10; i++)
+    l += i;
+  // изтриваме кратните на 3
+  int i = 0;
+  for(LinkedListIterator<int> it = l.begin(); it; it && ++(++it), i += 3) {
+    int x;
+    l.deleteAt(x, it++); // не можем да разчитаме на този итератор вече
+    Assert::AreEqual(x, i);
+  }
+  Assert::AreEqual(*l.begin(), 1);
+  Assert::AreEqual(*l.end(), 8);
+  i = 1;
+  for(LinkedListIterator<int> it = l.begin(); it; ++it, ++i) {
+    // прескачаме кратните на 3
+    if (i % 3 == 0)
+      ++i;
+    Assert::AreEqual(*it, i);
+  }
+}
+
+TEST_CASE("LinkedList", DeleteBeforeIsCorrect) {
+  LinkedList<int> l;
+  for(int i = 0; i < 10; i++)
+    l += i;
+  // ще изтрием четните числа
+  int i = 0;
+  for(LinkedListIterator<int> it = ++l.begin(); it; ++it && ++it, i += 2) {
+    int x;
+    l.deleteBefore(x, it);
+    Assert::AreEqual(x, i);
+  }
+  Assert::AreEqual(*l.begin(), 1);
+  // останали са само нечетните числа
+  i = 1;
+  for(LinkedListIterator<int> it = l.begin(); it; ++it, i += 2)
+    Assert::AreEqual(*it, i);
+}
+
+TEST_CASE("LinkedList", LinkedList_CopyAvoidsSharing) {
+  LinkedList<int>* l1 = new LinkedList<int>;
+  for(int i = 0; i < 10; i++)
+    l1->insertEnd(i);
+  LinkedList<int> l2 = *l1;
+  delete l1;
+  l2.insertBegin(-1);
+  l2.insertEnd(10);
+  int i = -1;
+  for(LinkedListIterator<int> it = l2.begin(); it; ++it, ++i)
+    Assert::AreEqual(*it, i);
+}
+
+TEST_CASE("LinkedList", LinkedList_AssignAvoidsSharing) {
+  LinkedList<int>* l1 = new LinkedList<int>;
+  for(int i = 0; i < 10; i++)
+    l1->insertEnd(i);
+  LinkedList<int> l2;
+  l2.insertBegin(100);
+  l2.insertEnd(200);
+  l2 = *l1;
+  delete l1;
+  l2.insertBegin(-1);
+  l2.insertEnd(10);
+  int i = -1;
+  for(LinkedListIterator<int> it = l2.begin(); it; ++it, ++i)
     Assert::AreEqual(*it, i);
 }
