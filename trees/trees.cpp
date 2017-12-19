8dc4dea9c095b0962b61dbf0fda92e8c404d4502
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-12-19 12:06:01 +0200

    Коментиране на тестовете за прегледност

--- trees/trees.cpp
+++ trees/trees.cpp
@@ -1,23 +1,24 @@
 #include <fstream>
 //#include "rtree_tests.h"
 #include "bintree_tests.h"
-#include "heap_tests.h"
+//#include "heap_tests.h"
+#include "bstree_tests.h"
 
 /*
 void RTreeDOTTest() {
   std::ofstream of("rtree.dot");
   createTestTree().printDOT(of);
   }
-*/
 
 void BinTreeDOTTest() {
   std::ofstream of("bintree.dot");
   createTestBinTree().printDOT(of);
 }
+*/
 
 int main() {
   //  RTreeDOTTest();
-  BinTreeDOTTest();
+  // BinTreeDOTTest();
   RunTests();
   return 0;
 }
